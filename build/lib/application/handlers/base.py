524a7375f462468f729ad239e92c145db2e660b1
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2013-06-20 20:00:24 -0500

    remove build

--- build/lib/application/handlers/base.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from tornado.web import RequestHandler, HTTPError
-
-
-class BaseHandler(RequestHandler):
-    """ Contains helper methods for all request handlers """
-    
-    pass
