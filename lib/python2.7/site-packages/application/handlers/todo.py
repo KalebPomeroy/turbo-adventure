b9eaf96b818e24cf0f345d689a558768bcb98159
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2013-06-20 20:00:59 -0500

    removed include and lib too

--- lib/python2.7/site-packages/application/handlers/todo.py
+++
@@ -1,8 +0,0 @@
-from application.lib.route import route
-from application.handlers.base import BaseHandler
-
-@route(r'/todo')
-class TodoHandler(BaseHandler):
-
-    def get(self):
-        self.write("foo")
